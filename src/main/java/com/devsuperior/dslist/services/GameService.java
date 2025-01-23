a5076644f1fc79ea1eb544127d18af2d7f2dd78f
Guilherme Ferreira <guilhermepereiraferreira4@gmail.com>
2025-01-23 00:41:51 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,24 +4,33 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 
-@Service//registro de componete do sistema //injetando 
+@Service // registro de componete do sistema //injetando
 public class GameService {
-	
+
 	@Autowired
 	private GameRepository gameRepository;
-	
-	public  List<GameMinDTO> findAll(){
-		var result = gameRepository.findAll();//List<Game>
-		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();//stream sequência de dados //map transforma
-		return dto;
-		//return result.stream().map(x -> new GameMinDTO(x)).toList();
+
+	@Transactional(readOnly = true) // garantir que a operação do bd aconteça
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+
 	}
-	
-	
-	
+
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();// List<Game>
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();// stream sequência de dados //map
+																					// transforma
+		return dto;
+		// return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
 
 }
